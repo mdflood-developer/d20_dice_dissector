471e6bbdff24516e449d7ca104a1191b3e3f4a18
Michael Flood <54719937+mdflood-developer@users.noreply.github.com>
2019-09-12 11:39:42 -0600

    Update d_dissector.py
    Function now returns dictionary with low and high values

--- d_dissector.py
+++ d_dissector.py
@@ -1,20 +1,31 @@
+# String parser for dice values in pen and paper RPGs like Pathfinder and Dungeons & Dragons.
+# Takes a value like '4d8+2' (which means, in game terms "Roll four eight-sided dice and add two to the final result")
+# and returns the values as individual integers in a dictionary
+
 def d_dissector(x):
     dstring = x
-    dstring_num = dstring.split('d')[0]
+    dstring_num = int(dstring.split('d')[0])
     dstring_dice = dstring.split('d')[1]
     if '+' in dstring_dice:
-        dstring_mult = dstring_dice.split('+')[0]
-        dstring_bonus = dstring_dice.split('+')[1]
+        dstring_mult = int(dstring_dice.split('+')[0])
+        dstring_bonus = int(dstring_dice.split('+')[1])
     elif '-' in dstring_dice:
-        dstring_mult = dstring_dice.split('-')[0]
-        dstring_bonus = dstring_dice.split('-')[1]
+        dstring_mult = int(dstring_dice.split('-')[0])
+        dstring_bonus = -int(dstring_dice.split('-')[1])
     else:
-        dstring_mult = dstring_dice
+        dstring_mult = int(dstring_dice)
         dstring_bonus = 0
-    print(dstring)
-    print(dstring_num)
-    print(dstring_dice)
-    print(dstring_mult)
-    print(dstring_bonus)
 
-d_dissector('4d8+2')
+    roll_low = (dstring_num * 1) + dstring_bonus
+    roll_high = (dstring_num * dstring_mult) + dstring_bonus
+
+    
+    dicevalue = {
+        'dice':dstring,
+        'num_dice':(dstring_num),
+        'num_value':(dstring_mult),
+        'num_bonus':(dstring_bonus),
+        'high':roll_low,
+        'low':roll_high
+        }
+    return dicevalue
